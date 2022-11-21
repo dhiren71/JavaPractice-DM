41da432903006ae601f15298b68ae1e298bd2636
Dhiren <talk2_dhiren@yahoo.com>
2022-11-20 19:51:15 -0500

    Initial Commit

---
+++ src/com/dhiren/HelloDhiren.java
@@ -0,0 +1,16 @@
+package com.dhiren;
+
+public class HelloDhiren {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Dhiren!");
+		long l = 42;
+		int i = (int)l;
+		System.out.println(i);
+
+	}
+	
+	long longvar = 42;
+
+}
